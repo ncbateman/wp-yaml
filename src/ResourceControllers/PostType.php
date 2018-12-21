960e56d14b9aa5ab9e7f8e56b094ec8bf5573614
Nicholas Bateman <ncbateman@gmail.com>
2018-12-21 19:07:19 +0000

    posttype and route controller done

--- src/ResourceControllers/PostType.php
+++ src/ResourceControllers/PostType.php
@@ -5,4 +5,11 @@ namespace WpYaml\ResourceControllers;
 class PostType extends ResourceController
 {
 
+  public function process()
+  {
+      foreach ( $this->config as $post_type => $args ) {
+          register_post_type($post_type, $args);
+      }
+  }
+
 }
