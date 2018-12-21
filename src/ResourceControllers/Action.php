18fcef9018c416b40e7d128b54b79b8aded558b8
Nicholas Bateman <ncbateman@gmail.com>
2018-12-21 23:18:58 +0000

    added action and filter controller gubbins

--- src/ResourceControllers/Action.php
+++ src/ResourceControllers/Action.php
@@ -6,6 +6,10 @@ class Action extends ResourceController
 {
   public function setup ()
   {
-
+    if ( isset( $this->config['actions'] ) && isset( $this->config['callback'] ) ) {
+      foreach ( $this->config['actions'] as $action ) {
+        add_action( $action['hook'], [ $this->config['callback'], $action['method'] ] );
+      }
+    }
   }
 }
--- src/ResourceControllers/Filter.php
+++ src/ResourceControllers/Filter.php
@@ -6,6 +6,10 @@ class Filter extends ResourceController
 {
   public function process ()
   {
-
+    if ( isset( $this->config['filters'] ) && isset( $this->config['callback'] ) ) {
+      foreach ( $this->config['filters'] as $action ) {
+        add_filter( $action['hook'], [ $this->config['callback'], $action['method'] ]  );
+      }
+    }
   }
 }
